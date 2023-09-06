33cac1eb6d462a915e69b7828bdd66cb58d40dd4
Toni01837463829 <107929691+Toni01837463829@users.noreply.github.com>
2023-09-06 11:14:29 +0200

    initial commit

---
+++ Model/Entities/Answers.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Model.Entities;
+[Table("ANSWERS")]
+public class Answers
+{
+    [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Column("ANSWER_ID")]
+    public int Id { get; set; }
+    
+    
+    [Column("ANSWER_TEXT")]
+    public string AnswerText { get; set; }
+    
+    [Column("ISCORRECT")]
+    public bool IsCorrect { get; set; }
+    
+    [Column("QUESTION_ID")]
+    public int QuestionId { get; set; }
+    public Questions Question { get; set; }
+}
---
+++ Model/Migrations/20230906085153_initcreate_06_09_2023.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Model.Migrations
+{
+    public partial class initcreate_06_09_2023 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "QUESTIONS",
+                columns: table => new
+                {
+                    QUESTION_ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    QUESTION_TEXT = table.Column<string>(type: "varchar(45)", maxLength: 45, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_QUESTIONS", x => x.QUESTION_ID);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "SCORES",
+                columns: table => new
+                {
+                    QUESTION_ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    PLAYER_NAME = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    SCORE = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SCORES", x => x.QUESTION_ID);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "ANSWERS",
+                columns: table => new
+                {
+                    ANSWER_ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    ANSWER_TEXT = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    ISCORRECT = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    QUESTION_ID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ANSWERS", x => x.ANSWER_ID);
+                    table.ForeignKey(
+                        name: "FK_ANSWERS_QUESTIONS_QUESTION_ID",
+                        column: x => x.QUESTION_ID,
+                        principalTable: "QUESTIONS",
+                        principalColumn: "QUESTION_ID",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ANSWERS_QUESTION_ID",
+                table: "ANSWERS",
+                column: "QUESTION_ID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ANSWERS");
+
+            migrationBuilder.DropTable(
+                name: "SCORES");
+
+            migrationBuilder.DropTable(
+                name: "QUESTIONS");
+        }
+    }
+}
