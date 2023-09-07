6ba93227aa61c9305c01920ac703e25d33175c75
Christian Elmar Michael Wiesinger <C.WIESINGER@htlkrems.at>
2023-09-07 10:58:53 +0200

    UserInfo Functions added

--- GUI/Data/UserInformation.cs
+++ GUI/Data/UserInformation.cs
@@ -8,5 +8,30 @@ namespace GUI
         public int CurrentScore { get; set; } = 0;
         public List<int> previousQuestionIDs { get; set; } = new List<int>();
         public int MomentaryHighScore { get; set; } = 0; //just used to prevent uploading every score just the highest from each user 
+
+
+        public void Reset ()
+        {
+            Name = "Guest";
+            CurrentScore = 0;
+            previousQuestionIDs = new List<int>();
+            MomentaryHighScore = 0;
+        }
+
+        /// <summary>
+        /// Execute when the player has answered a wrong Questions
+        /// </summary>
+        public bool Lost()
+        {
+            previousQuestionIDs.Clear();
+            if(CurrentScore > MomentaryHighScore)
+            {
+                MomentaryHighScore = CurrentScore;
+
+                //Change Highscore in DB
+            }
+
+            CurrentScore = 0;
+        }
     }
 }
